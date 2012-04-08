02d35bd6d3a4a46f8144e9824601fe287538e572
MiniRagnarok <Jimmy@Zalera.(none)>
2012-04-07 21:20:50 -0400

    .

--- GameBoardImpl.java
+++ GameBoardImpl.java
@@ -1,10 +1,11 @@
 public class  GameBoardImpl implements GameBoard{
-	private int[][] GameBoard = new int[6][7];
- 
-	public int[][] GetGameBoard(){
-	  return GameBoard;
+  private int[][] gameboard = new int[6][7];
+
+  public int[][] GetGameBoard(){
+    return gameboard;
   }
 }
 
 interface GameBoard{
+  public int[][] GetGameBoard();
 }
--- GameEngineImpl.java
+++ GameEngineImpl.java
@@ -1,7 +1,10 @@
-public class  GameEngineImpl{
+public class  GameEngineImpl implements GameEngine{
   public static void main(String[] args){
+    void GameLoop(){
+    }
   }
 }
 
 interface GameEngine{
+  void GameLoop();
 }
