84d525510420a07697e32696676ae2bdabe435bf
unknown <Jimmy@Zalera.(none)>
2012-04-07 20:41:28 -0400

    Testing Asserts.

--- TestDisplay.java
+++ TestDisplay.java
@@ -4,7 +4,12 @@ import static org.junit.Assert.*;
 public class TestDisplay{
   @Test public void testdrawmenu(){
     Display display = new DisplayImpl();
-    assertTrue(ReDrawGameBoard(log[], gameboard[][]);
-    assertTrue(display.DrawMenu());
+    
+    assertTrue(ReDrawGameBoard(log[], gameboard[][]), true);
+    assertTrue(display.DrawMenu(), true);
+    
+    assertNotNull(ReDrawGameBoard(log[], gameboard[][]));
+    assertNotNull(display.DrawMenu());
+    
   }
 }
