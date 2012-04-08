2d81902c43b0ddf769d31dd4239662f3be13b922
unknown <Jimmy@Zalera.(none)>
2012-04-07 20:10:50 -0400

    Playing around

---
+++ TestDisplay.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestDisplay{
+  @Test public void testdrawmenu(){
+    Display display = new DisplayImpl();
+    assertTrue(ReDrawGameBoard(log[], gameboard[][]);
+    assertTrue(display.DrawMenu());
+  }
+}
