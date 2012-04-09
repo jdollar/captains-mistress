98cc528dd169400d6eb3ccbc812d880111e90880
MiniRagnarok <Jimmy@Zalera.(none)>
2012-04-08 21:20:40 -0400

    .

--- DisplayImpl.java
+++ DisplayImpl.java
@@ -1,13 +1,11 @@
 public class  DisplayImpl implements Display{
-  public static void main(String[] args){
-  }
   public void ReDrawGameBoard(int[] log, int[][] gameboard){
   }
-  public void DrawMenu(int[][] gameboard){
+  public void DrawMenu(){
   }
 }
 
 interface Display{
   public void ReDrawGameBoard(int[] log, int[][] gameboard);
-  public void DrawMenu(int[][] gameboard);
+  public void DrawMenu();
 }
