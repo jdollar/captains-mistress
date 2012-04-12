033952940aa50b6f739cc5330641e0cc017ab0e6
unknown <Shorty@Shorty-HP.(none)>
2012-04-12 13:06:24 -0400

    added move checker

--- UserInput.java
+++ UserInput.java
@@ -1,11 +1,33 @@
 import java.util.*;
 
 public class UserInput extends GeneralControlsImpl {
-	private int PlayerMove;
+	private int playerMove;
+	private int moveCheck = 0;
+	
+	UserInput(int[][] GameBoardPass){
+		super(GameBoardPass);
+	}
 	public int GetInput(){
 		System.out.println ("Please make a move");
-		PlayerMove = System.in;
+		Scanner in = new Scanner(System.in);
+		playerMove = in.nextInt();
+		if (playerMove >= 1 || playerMove <=  7)
+		{
+			moveCheck = 1;
+		}
+		
+		while (moveCheck != 1)
+		{
+			System.out.println ("Not a valid move, please select a new move");
+			playerMove = in.nextInt();
+			if (playerMove >= 1 || playerMove <=  7)
+			{
+				moveCheck = 1;
+			}
+		}
+		
+		in.close();
 
-		return PlayerMove;
+		return playerMove;
 	}
 }
