e561f9f4ab3fb4855ee0e74b51b2d6271bf3c896
MiniRagnarok <Jimmy@Zalera.(none)>
2012-04-12 19:52:36 -0400

    .

--- James/DisplayImpl.java
+++ James/DisplayImpl.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class  DisplayImpl implements Display{
   public void ReDrawGameBoard(int[] log, int[][] gameboard){
   }
   public void DrawMenu(){
-  	System.out.println("");
-  	System.out.println("Main Menu");
-  	System.out.println("");
-  	System.out.println("1)Play");
-  	System.out.println("2)Instructions");
-  	System.out.println("3)Quit Game");
+    int userInput = 0;
+    Scanner input = new Scanner (System.in);
+
+    while (userInput != 1 || 3){
+      System.out.println("");
+      System.out.println("Main Menu");
+      System.out.println("");
+      System.out.println("1)Play");
+      System.out.println("2)Instructions");
+      System.out.println("3)Quit Game");
+      
+      switch(userInput){
+        case 1:  GameEngine();
+                 break;
+        case 2:  ;
+                 break;
+        case 3:  System.exit(0);
+                 break;
+        default: System.out.println("Invalid Data, please try again");
+                 break;
+    }
+
+
+
   }
   public static void main(String[] args){
   	DisplayImpl link = new DisplayImpl();
