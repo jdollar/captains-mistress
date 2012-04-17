121806e74c6de8fa64a601a5f9accd14f0b0d613
unknown <zhershbe@ne132026.(none)>
2012-04-17 14:34:44 -0400

    .

--- Zach/GameEngineImpl.java
+++ Zach/GameEngineImpl.java
@@ -3,13 +3,46 @@ package Zach;
 import mainPack.*;
 
 public class  GameEngineImpl implements GameEngine{
+	int playerToken;
   public void GameLoop(){
+    boolean loopcondition = true;
+    int choice = 0;
   	StateCheckImpl loopChecks = new StateCheckImpl();
+    DisplayImpl gameDisplay = new DisplayImpl();
 	DummyGameBoardImpl checkBoard = new DummyGameBoardImpl();
 	DummyControl player = new DummyControl(checkBoard);
-  	while(!loopChecks.CheckVictory(checkBoard.getGameBoard())){
-		player.getInput();
+  	while (loopcondition){
+	if (choice == 1)
+	{
+	 while(!loopChecks.CheckVictory(checkBoard.getGameBoard())){
+		gameDisplay.ReDrawGameBoard(checkBoard); //Redraws game board
+		int input = player.getInput();           
+		checkBoard.setValue(input, playerToken);
+		if (loopChecks.CheckVictory(checkBoard))
+		{
+			// Calls displays output for victory
+			break;
+		}
+		else if (loopChecks.CheckDraw(checkBoard))
+		{
+			// Calls displays output for draw
+			break;
+		}
+		
   	}
+	}
+    else if (choice == 2){
+		// call display instructions
+	}
+	else if (choice == 4){
+		loopcondition =  false;
+		}
+	else {
+		System.out.println("Invaild choice.");
+	}
+	}
+
   }
+  
 }
 
