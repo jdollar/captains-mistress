e44c0a6eb5956b09b240fa8692d3090d6ae8d728
Joel Dollarhide <ionizedwarhead10@gmail.com>
2012-04-18 23:01:39 -0400

    Created new branch for testing

--- Joel/SmartAIImpl.java
+++ Joel/SmartAIImpl.java
@@ -5,6 +5,10 @@ import mainPack.*;
 
 public class SmartAIImpl extends AI implements SmartAI{
 	private final int STEPS = 4;
+	enum Direction {
+		UP, DOWN, LEFT, RIGHT, RIGHTUP, RIGHTDOWN, LEFTUP, LEFTDOWN, NONE
+	}
+	
 	public SmartAIImpl(DummyGameBoardImpl GameBoardPass){
 		super(GameBoardPass);
 	}
@@ -83,54 +87,104 @@ public class SmartAIImpl extends AI implements SmartAI{
 	}
 	public int TokenCount(int xValue, int yValue, int x2Value, int y2Value, int player){
 		int tempScore = 0;
-//		System.out.println("x2: " + x2Value);
-		System.out.println("x: " + xValue);
-//		System.out.println("y2: " + y2Value);
-		System.out.println("y: " + yValue);
-//		System.out.println("COL:" + GameBoard.getNumColumns());
-//		System.out.println("ROW: " + GameBoard.getNumRows());
+		boolean multipleTokens = false;
+		Direction heading = Direction.NONE;
+
 		if(x2Value < gameBoard.getNumColumns() && x2Value >= 0 && y2Value < gameBoard.getNumRows() && y2Value >= 0
 				&& xValue < gameBoard.getNumColumns() && yValue < gameBoard.getNumRows()){
-			System.out.println("True");
-			System.out.println("x2: " + x2Value);
-			System.out.println("y2: " + y2Value);
-			System.out.println("P: " + player);
-			System.out.println("G: " + gameBoard.getValue(y2Value, x2Value));
+			
 			if(gameBoard.getValue(y2Value, x2Value) == player){
 				System.out.println("False");
 				if(yValue < y2Value && xValue  > x2Value){
+					heading = Direction.LEFTUP;
 					tempScore += TokenCount(x2Value, y2Value, x2Value - 1, y2Value + 2, player); //left up diagonal
 				}
 				if(yValue > y2Value && xValue < x2Value){
+					heading = Direction.RIGHTDOWN;
 					tempScore += TokenCount(x2Value, y2Value, x2Value + 1, y2Value + 2, player); //right down diagonal
 				}
 				if(xValue < x2Value && yValue < y2Value){
+					heading = Direction.RIGHTUP;
 					tempScore += TokenCount(x2Value, y2Value, x2Value + 1, y2Value - 2, player);  //right up diagonal
 				}
 				if(xValue > x2Value && yValue > y2Value){
+					heading = Direction.LEFTDOWN;
 					tempScore += TokenCount(x2Value, y2Value, x2Value - 1, y2Value + 2, player);  //left down diagonal
 				}
 				if(xValue == x2Value && yValue > y2Value){
+					heading = Direction.UP;
 					tempScore += TokenCount(x2Value, y2Value, x2Value, y2Value - 1, player);  //up
 				}
 				if(xValue == x2Value && yValue < y2Value){
+					heading = Direction.DOWN;
 					tempScore += TokenCount(x2Value, y2Value, x2Value, y2Value + 1, player);  //down
 				}
 				if(xValue < x2Value && yValue == y2Value){
+					heading = Direction.RIGHT;
 					tempScore += TokenCount(x2Value, y2Value, x2Value + 1, y2Value, player); //right
 				}
 				if(xValue > x2Value && yValue == y2Value){
+					heading = Direction.LEFT;
 					tempScore += TokenCount(x2Value, y2Value, x2Value - 1, y2Value, player); //left
 				}
+				
+				//return tempScore + 1;
 			}
 		}
 		
-		if(yValue < gameBoard.getNumRows() - 1){
-			if(gameBoard.getValue(yValue + 1, xValue) == player){
+		/*if(y2Value < gameBoard.getNumRows() - 1 && y2Value >= 0 &&
+				x2Value < gameBoard.getNumColumns() && x2Value >= 0){
+			switch(heading){
+			case UP:
+				if(gameBoard.getValue(yValue - 1, xValue) == player){
+					multipleTokens = true;
+				}
+				break;
+			case DOWN:
+				if(gameBoard.getValue(yValue + 1, xValue) == player){
+					multipleTokens = true;
+				}
+				break; 
+			case LEFT:
+				if(gameBoard.getValue(yValue, xValue - 1) == player){
+					multipleTokens = true;
+				}
+				break;
+			case RIGHT:
+				if(gameBoard.getValue(yValue, xValue + 1) == player){
+					multipleTokens = true;
+				}
+				break;
+			case RIGHTUP:
+				if(gameBoard.getValue(yValue - 2, xValue + 1) == player){
+					multipleTokens = true;
+				}
+				break;
+			case RIGHTDOWN:
+				if(gameBoard.getValue(yValue + 2, x2Value - 1) == player){
+					multipleTokens = true;
+				}
+				break;
+			case LEFTUP:
+				if(gameBoard.getValue(yValue - 2, xValue - 1) == player){
+					multipleTokens = true;
+				}
+				break;
+			case LEFTDOWN:
+				if(gameBoard.getValue(yValue + 2, xValue - 1) == player){
+					multipleTokens = true;
+				}
+				break;
+			}
+			
+			System.out.println(multipleTokens);
+			if(multipleTokens){
 				return tempScore + 1;
 			}
+		}*/
+		if(gameBoard.getValue(yValue, xValue) == player){
+			return tempScore + 1;
 		}
-		
-		return 0;
+		return tempScore;
 	}
 }
--- Test/SmartAIImplTest.java
+++ Test/SmartAIImplTest.java
@@ -25,24 +25,40 @@ public class SmartAIImplTest {
 		testBoard.columnNTokens(3, 1, 0);
 		testBoard.displayBoard();
 		tester = new SmartAIImpl(testBoard);
-		assertEquals("Result", 3, tester.TokenCount(0, 2, 0, 3, 1));
+		assertEquals("Result", 3, tester.TokenCount(0, testBoard.getLowestGridValue(0), 0, 3, 1));
+		assertEquals("Result", 1, tester.TokenCount(1, 2, 0, 3, 1));
 		
 		testBoard.columnNTokens(4, 1, 1);
 		testBoard.displayBoard();
 		assertEquals("Result", 4, tester.TokenCount(1, 1, 1, 2, 1));
 		assertEquals("Result", 2, tester.TokenCount(1, 1, 0, 3, 1));
+		
+		testBoard.emptyGameBoard();
+		testBoard.columnNTokens(1, 1, 0);
+		testBoard.columnNTokens(1, 1, 1);
+		testBoard.columnNTokens(1, 1, 2);
+		assertEquals("Result", 3, tester.TokenCount(3, 0, 2, 0, 1));
+		
+		testBoard.emptyGameBoard();
+		testBoard.fullGameBoardToken(1);
+		assertEquals("Result", 5, tester.TokenCount(0, 0, 0, 1, 1));
+		
+		testBoard.emptyGameBoard();
+		testBoard.columnNTokens(4, 1, 0);
+		testBoard.columnNTokens(3, 1, 1);
+		assertEquals("Result", 2, tester.TokenCount(0, 1, 1, 3, 1));
 	}
 	
-	@Test
-	public void testScoreDetermine(){
-		testBoard.emptyGameBoard();
-		testBoard.displayBoard();
-		System.out.println("\n");
-		testBoard.columnNTokens(3, 1, 0);
-		testBoard.displayBoard();
-		tester = new SmartAIImpl(testBoard);
-		assertEquals("Result", 3, tester.ScoreDetermine(1));
-	}
+//	@Test
+//	public void testScoreDetermine(){
+//		testBoard.emptyGameBoard();
+//		testBoard.displayBoard();
+//		System.out.println("\n");
+//		testBoard.columnNTokens(3, 1, 0);
+//		testBoard.displayBoard();
+//		tester = new SmartAIImpl(testBoard);
+//		assertEquals("Result", 3, tester.ScoreDetermine(1));
+//	}
 //	@Test
 //	public void testScoreDetermine() {
 //		fail("Not yet implemented");
