3b99f175792d38adb155580eb22a40cccd5257bc
Joel Dollarhide <ionizedwarhead10@gmail.com>
2012-04-20 19:39:53 -0400

    .

--- Joel/NTreeImpl.java
+++ Joel/NTreeImpl.java
@@ -26,37 +26,36 @@ public class NTreeImpl implements NTree{
 		if(board.checkValid(column)){
 			if(player != -1){
 				//if not the root node change the value of the current column
-				board.setValue(board.getLowestGridValue(column), column, player);
+				tempBoard.setValue(tempBoard.getLowestGridValue(column), column, player);
 			}
 		}
 
 		//creates an instance of the SmartAI for the score determination.
-		tests = new SmartAIImpl(board);
+		tests = new SmartAIImpl(tempBoard);
 
 		//as long as max steps haven't been reached check to see how many children
 		//are possible and input moves possible into an array so when buildtree is
 		//called it knows which column to use.
-
-		if(depth <= stepCount + 1){
-			for(int x = 0; x < board.getNumColumns(); x++){
-				if(board.checkValid(x)){
+		if(depth <= stepCount){
+			for(int x = 0; x < tempBoard.getNumColumns(); x++){
+				if(tempBoard.checkValid(x)){
 					validMoveArray[validMoves] = x;
 					validMoves++;
 				}
 			}
 		}
-
 		//creates a node for the tree based on score of current position and how many different children
 		//it can make. Also stores the current player and depth of the node in the tree
 		NodeImpl n = new NodeImpl(tests.ScoreDetermine(player), validMoves, player, depth);
 
-		if(depth <= stepCount + 1){
+		if(depth <= stepCount){	
 			for(int i = 0; i < validMoves; i++){
-					//creates a child with new gameboard created here and places it in child of current node
-					depth++;
-					tempBoard.displayBoard();
-					n.setChildAt(i, buildTree(tempBoard, changePlayer(player), validMoveArray[i]));
-					depth--;
+				//creates a child with new gameboard created here and places it in child of current node
+				depth++;
+				tempBoard.displayBoard();
+				n.setChildAt(i, buildTree(tempBoard, changePlayer(player), validMoveArray[i]));
+				tempBoard = board;
+				depth--;
 			}
 		}
 
