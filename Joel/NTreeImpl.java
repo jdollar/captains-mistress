fee4920b00da6a7aabb288d0a800d612091fd616
Joel Dollarhide <ionizedwarhead10@gmail.com>
2012-04-21 13:34:48 -0400

    Cleaned up Code tried to debug and added additional comments

--- Joel/NTreeImpl.java
+++ Joel/NTreeImpl.java
@@ -19,19 +19,22 @@ public class NTreeImpl implements NTree{
 	}
 
 	public NodeImpl buildTree(DummyGameBoardImpl board, int player, int column){
+		//variable declaration
 		int validMoves = 0;
 		int[] validMoveArray = new int[7]; 
+		
+		/*creates a new instance of the board class to hold this node as a 
+		 *parent gameBoard reference for the children so they should
+		 *restart their states with the parent gameBoard then explore
+		 *all possible choices*/
 		DummyGameBoardImpl tempBoard = new DummyGameBoardImpl(board);
 
-		//checks if move possible on column and if not root node
-
-
-		//creates an instance of the SmartAI for the score determination.
+		//creates an instance of the StateChecker for the score determination.
 		tests = new StateCheckImpl(board);
 
-		//as long as max steps haven't been reached check to see how many children
-		//are possible and input moves possible into an array so when buildtree is
-		//called it knows which column to use.
+		/*as long as max steps haven't been reached check to see how many children
+		 *are possible and input moves possible into an array so when buildtree is
+		 *called it knows which column to use.*/
 		if(depth <= stepCount){
 			for(int x = 0; x < board.getNumColumns(); x++){
 				if(board.checkValid(x)){
@@ -40,8 +43,8 @@ public class NTreeImpl implements NTree{
 				}
 			}
 		}
-		//creates a node for the tree based on score of current position and how many different children
-		//it can make. Also stores the current player and depth of the node in the tree
+		/*creates a node for the tree based on score of current position and how many different children
+		 *it can make. Also stores the current player and depth of the node in the tree*/
 		NodeImpl n = new NodeImpl(tests.scoreDetermine(player), validMoves, player, depth);
 
 		if(depth <= stepCount){	
@@ -49,17 +52,28 @@ public class NTreeImpl implements NTree{
 			for(int i = 0; i < validMoves; i++){
 				//creates a child with new gameboard created here and places it in child of current node
 				depth++;
+				
+				/*done for testing. Getting weird errors with array not populating right
+				 *Placed board set value inside this loop so that board is explicitly
+				 *(via assignment statement below) getting reinitialized as original
+				 *board value then assigned one by one.*/
 				board.displayBoard();
 				System.out.println();
 				board = tempBoard;
+				
+				//checks if move possible on column and if not root node
 				if(board.checkValid(column)){
 					if(player != -1){
 						//if not the root node change the value of the current column
 						board.setValue(board.getLowestGridValue(validMoveArray[i]), validMoveArray[i], player);
 					}
 				}
+				
+				/*should create a child with one of the valid moves recursively running this method
+				 *again until depth has been reached then go out one layer and do the next child
+				 *of that node until we get all children of root'ss first 7 children for stepCount
+				 *layers*/
 				n.setChildAt(i, buildTree(new DummyGameBoardImpl(board), changePlayer(player), validMoveArray[i]));
-				//tempBoard = board;
 				depth--;
 			}
 		}
