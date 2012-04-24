8d2b9e3f1900e787da52f5532fe86dbb2e699769
MiniRagnarok <Jimmy@Zalera.(none)>
2012-04-24 10:05:55 -0400

    .

--- compile.bat
+++ compile.bat
@@ -4,6 +4,5 @@ javac .\Josh\*.java
 javac .\Zach\*.java
 javac .\Mohammed\*.java
 javac .\mainPack\*.java
-javac .\Test\*.java
 
 jar cfm captainsMistress.jar manifest.txt .\James\*.class .\Joel\*.class .\Josh\*.class .\Zach\*.class .\Mohammed\*.class 
